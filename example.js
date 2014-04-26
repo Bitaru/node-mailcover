c883cb3bd195c1bad4a4dc26b10950e39f83a93b
bitaru <aiir.diir@gmail.com>
2014-04-26 15:47:15 +0400

    Git init

---
+++ example.js
@@ -0,0 +1,16 @@
+var mailcover = require('./mailcover');
+
+mailcover.config = {
+
+    server: 'us1',
+    key: '4838c775538ffefa01e3fcaf478c7cce',
+    blockDEA: false,
+    user:{
+        user_ip: '127.0.1.2',
+        user_agent: 'Mozilla 5.0'
+    }
+
+};
+mailcover.validate('aiir.d22iir@gmail.com', function(response){
+    console.log(response);
+});
---
+++ mailcover.js
@@ -0,0 +1,121 @@
+/*!
+ * Copyright (c) 2014 Bitaru <web@mailcover.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+(function (name, definition) {
+
+    module.exports = definition();
+
+})('mailcover', function (mailcover) {
+
+    'use strict';
+
+    var https = require('https'),
+
+        pattern = /^([^@]+)@([^@]+)\.([^@]+)$/;
+
+    // Extend Validator isEmail function if exist
+    mailcover = {};
+
+    mailcover.messages = {
+
+        blank: 'Email must not be blank',
+        incorrect: 'Email is invalid'
+
+    };
+
+    /**
+     * Main validation method
+     * @param {string} email to validate
+     * @param {object}
+     * @param {function} callback return string if email is incorrect
+     * @return {bool}
+     */
+    mailcover.validate = function(email, callback){
+
+        // NotBlank checker
+        if(email.replace(' ', '') == ''){
+            callback(messages.blank);
+        }
+
+        // Simple syntax validation
+        if(!pattern.test(email)){
+            callback(messages.blank);
+        }else{
+
+            var connect, req, config, messages;
+
+            config = this.config;
+            messages = this.messages;
+
+            connect = {
+                hostname: config.server+'.api.mailcover.com',
+                port: 443,
+                path: '/'+config.key + '/private/' + encodeURIComponent(email) +'/json',
+                method: 'POST',
+                headers:{
+                    'Content-type': 'application/x-www-form-urlencoded'
+                }
+            };
+
+            req = https.request(connect, function(response){
+                var str = '';
+
+                response.on('data', function (chunk) {
+                    str += chunk;
+                });
+
+                response.on('end', function () {
+                    var data = JSON.parse(str);
+                    if(config.blockDEA && data.is_disposable){
+                        callback(messages.incorrect);
+                    }
+                    if(data.status == 'invalid'){
+                        callback(messages[data.reason] || data.reason);
+                    }
+                });
+            });
+
+            req.write( JSON.stringify(mailcover.config.user) || '', 'utf8' );
+            req.end();
+
+        }
+
+    };
+
+
+    /*
+    * Helpers
+    * */
+    var extend = function(target) {
+        var sources = [].slice.call(arguments, 1);
+        sources.forEach(function (source) {
+            for (var prop in source) {
+                target[prop] = source[prop];
+            }
+        });
+        return target;
+    }
+
+    return mailcover;
+
+});
